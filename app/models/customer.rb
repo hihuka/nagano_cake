d045e10be5b6ec56d683b0132d77c582df7043f6
EC2 Default User <ec2-user@ip-172-31-57-75.ec2.internal>
2022-11-04 04:26:12 +0000

    nagano_cake2

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
@@ -17,12 +17,12 @@ class Customer < ApplicationRecord
   validates :address, presence: true
   validates :telephone_number, presence: true, format: { with: /\A[0-9]+\z/, message: "は半角数字のみで入力して下さい" }
 
-  def active_for_authentication?
-    super && self.is_deleted == false
-  end
-  
-    def full_name
-		  self.first_name + self.last_name
-	  end
-	  
+   def active_for_authentication?
+     super && self.is_deleted == false
+   end
+
+   def full_name
+	  self.first_name + self.last_name
+	 end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,57 +1,44 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  devise_for :order_details
-  devise_for :orders
-  devise_for :genres
-  devise_for :items
-  devise_for :cart_items
-  devise_for :addresses
-  devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to:'homes#top'
+  get '/home/about' => 'homes#about'
 
- root to: "homes#top"
-  get 'homes/top'
-  get 'home/about' => 'homes#about'
+  get '/search' => 'search#search'
 
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: 'public' do
+
+    #onlyでアクションを指定してやらないとユーザー登録・ログイン時にcustomersコントローラーが呼び出されてしまう
+    resource :customers, only: [:edit, :update] do
+      get 'my_page' => 'customers#show'
+      get 'confirm'
+      patch 'withdrawal'
+    end
+
+    #下記の文が上記のresource :customers doよりも上にあるとcustomersコントローラーがうまく呼び出されない
+    #下記のコードがユーザー登録・ログインで使いたいコントローラー
+    devise_for :customers, only: [:sessions, :registrations]
+
+    resources :addresses
+
+    post "orders/thanks" => "orders#thanks"
+    get "orders/complete" => "orders#complete"
+    resources :orders, only:[:new, :create, :index, :show]
+    resources :cart_items, only:[:index, :update, :create, :destroy]
+    delete "cart_items" => "cart_items#destroy_all"
+    resources :items, only:[:index, :show]
+  end
+
+  devise_for :admins, only: [:sessions], :controllers => {
+    :sessions => 'admin/sessions'
+  }
+
   namespace :admin do
     resources :customers
     resources :items
     resources :genres, only:[:index, :update, :create, :edit]
     resources :orders, only:[:index, :show, :update]
     resources :order_details, only:[:update]
-
-    get 'homes/top'=> 'homes#admin'
+    get "orders/current_user_order/:id" => "orders#current_user_order"
   end
 end
